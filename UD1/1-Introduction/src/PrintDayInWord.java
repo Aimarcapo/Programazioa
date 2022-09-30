71260579337083d1d274acbc4a2bfe1199a1dd24
Alonso Toledano <alonso.aimar@uni.lan>
2022-09-30 09:57:14 +0200

    Si

---
+++ UD1/1-Introduction/src/PrintDayInWord.java
@@ -0,0 +1,29 @@
+public class PrintDayInWord {
+    
+    public static void main(String[] args) {
+        int day=2;  // Set the value of "number" here!
+   
+        
+   
+        // Using switch-case-default
+        switch(day) {
+           case 1: 
+              System.out.println( "Monday" ); break;  // Don't forget the "break" after each case!
+           case 2: 
+              System.out.println( "Tuesday" ); break;
+            case 3: 
+              System.out.println( "Wedsnay" ); break;
+            case 4: 
+              System.out.println( "Thursday" ); break;
+            case 5: 
+              System.out.println( "Friday" ); break;
+            case 6: 
+              System.out.println( "Saturday" ); break;
+            case 7: 
+              System.out.println( "Sunday" ); break;
+            
+           
+    
+        }
+     }
+}
