53a86e168ec45b9981efd2dc52a30e730ea36c03
John Doe <aimaralto943@gmail.com>
2022-10-16 17:33:48 +0200

    si

--- UD1/1-Introduction/src/CountVowelDigits.java
+++ UD1/1-Introduction/src/CountVowelDigits.java
@@ -1,12 +1,36 @@
 import java.util.Scanner;
-public class CountVowelDigits {
-    
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-            System.out.print("Enter a String:  ");
-            String c = input.nextLine();
-            input.close();
-            char c=""
-            System.out.println(
-                c + " is a digit -> "
-                + Character.isDigit(c));
+  
+public class CountAlphabetsInWord {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the word to count Vowels and Alphabets in it");
+        String word = scanner.nextLine();
+        scanner.close();
+        int vowels = 0 int consonants = 0, int spaces = 0, int digits = 0, int specialCharacters = 0;
+        word = word.toLowerCase();
+        for(int i = 0; i <= word.length(); i++){
+            char ch = word.charAt(i);
+            
+            //check if any char is a, e, i, o ,u
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                vowels=vowels+1;
+            }
+            else if(ch >=  '0' && ch <= '9'){
+                consonants++;
+            }
+            else if(ch == ' '){
+                spaces++;
+            }
+            else if(ch >= 'a' && ch <= 'z'){
+                digits++;
+            }
+            else
+                specialCharacters++;
+        }
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        System.out.println("Digits: " + digits);
+        System.out.println("WhiteSpaces: " + spaces);
+        System.out.println("Special Characters: " + specialCharacters);
+    }
+} 
