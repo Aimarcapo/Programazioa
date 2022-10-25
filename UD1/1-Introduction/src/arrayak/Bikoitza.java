bd2c41f65b078f6370c24c5071fef76f62ba0d4c
Alonso Toledano <alonso.aimar@uni.lan>
2022-10-25 14:25:03 +0200

    updatee

--- UD1/1-Introduction/src/arrayak/Bikoitza.java
+++ UD1/1-Introduction/src/arrayak/Bikoitza.java
@@ -1,43 +1,30 @@
 package arrayak;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Bikoitza {
 
     public static void main(String[] args) {
-        int numItems;
-        int[] items;
+        final int TAMAINA = 5;
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the number of items: ");
-        numItems = input.nextInt();
-        items = new int[numItems];
+        Scanner in = new Scanner(System.in);
+        int[] zenbakiak = new int[TAMAINA];
+        int[] bikoitzak = new int[TAMAINA];
 
- 
-        if (items.length > 0) {
-      
-           for (int i = 0; i < items.length; ++i) {
-              System.out.print("Enter the value of all items separated  : ");
-              items[i] = input.nextInt();
-              
-           }
-           
-               
-               
-            
-           for (int i = 0; i < items.length; ++i) {
-            if (i == 0) {
-               
-               System.out.print("The values are: [" + items[i] + "]");
-            } else {
-               
-               System.out.println("The values are: [" + items[i] + "]");
-            }
-            System.out.println("The values are: [" + items[i] + "]");
-            }
-            
-         }
-       
+        System.out.print("Sartu itzazu" + TAMAINA + "Zenbakiak espaziosz banaduta:");
+
+        for (int i = 0; i < TAMAINA; ++i) {
+            zenbakiak[i] = in.nextInt();
+
         }
-    
+        in.close();
+
+        for (int i = 0; i < TAMAINA; ++i) {
+            bikoitzak[i] = zenbakiak[i] * 2;
+        }
+        System.out.println(Arrays.toString(zenbakiak));
+        System.out.println(Arrays.toString(bikoitzak));
     }
+
+}
