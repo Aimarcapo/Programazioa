0ca8050ed5f9431c5c47bfee4f8c3331762f096d
John Doe <aimaralto943@gmail.com>
2022-10-29 12:09:02 +0200

    bai

--- UD1/1-Introduction/src/arrayak/prueba.java
+++ UD1/1-Introduction/src/arrayak/prueba.java
@@ -1,53 +1,21 @@
 package arrayak;
 
-import java.util.Scanner;
 
 public class prueba {
 
+ 
     public static void main(String[] args) {
-
-        final String[] HEX_BITS = { "0000", "0001", "0010", "0011",
-                "0100", "0101", "0110", "0111",
-                "1000", "1001", "1010", "1011",
-                "1100", "1101", "1110", "1111" };
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter a Hexadecimal string: ");
-        String hexStr = in.next();
-        Hex2Bin aHex2Bin = new Hex2Bin();
-        if ( ! aHex2Bin.isHex(hexStr)) {
-            System.out.printf("Invalid Hexadecimal string \"%1$s\"\n", hexStr);
-            return;
-        }
-        aHex2Bin.convertHexToBin(hexStr);
+       int number = 1;
+       while(true) {
+          ++number;
+          if ((number % 3) == 0) continue;
+          if (number == 133) break;
+          if ((number % 2) == 0) {
+             number += 3;
+          } else {
+             number -= 3;
+          }
+          System.out.print(number + " ");
+       }
     }
-    
-    /**
-     * 
-     *
-     */
-    private boolean isHex(String hexStr)
-    {
-        for (int i = 0; i < hexStr.length(); i++) {
-            if (0 > Character.digit(hexStr.toLowerCase().charAt(i), 16)) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    /**
-     * 
-     * 
-     */
-    private void convertHexToBin(String hexStr)
-    {
-        
-        System.out.printf("The equivalent binary for hexadecimal %1$s is ", hexStr);
-        
-        for (int i = 0; i < hexStr.length(); i++) {
-            int j = Character.digit(hexStr.toLowerCase().charAt(i), 16);
-            System.out.printf("%1$s ", hexStr[j]);
-        }
-        System.out.println();
-    }
-}
+ }
