708fe831496b76e8777adac73cb63a5ca2583d83
Alonso Toledano <alonso.aimar@uni.lan>
2022-12-05 14:24:15 +0100

    si

---
+++ UD1/1-Introduction/UD2/00-ARIKETAGEHIGARRIAK/src/Prog1.java
@@ -0,0 +1,7 @@
+public class Prog1 {
+    public static void main(String[] args) {
+        int[] zenbakiak={1,2,3};
+        System.out.println(zenbakiak[3]);
+
+    }
+}
---
+++ UD1/1-Introduction/UD2/00-ARIKETAGEHIGARRIAK/src/Prog2.java
@@ -0,0 +1,13 @@
+public class Prog2 {
+    
+    public static void main(String[] args) {
+        int[] zenbakiak = {1, 15, 2};
+        try {
+            System.out.println(zenbakiak[8]);
+        } catch (Exception ex) {
+            System.out.println("Salbuespena gertatu da.");
+            System.out.println(ex.getMessage());
+            System.out.println(ex.getClass());
+        }
+    }
+}
