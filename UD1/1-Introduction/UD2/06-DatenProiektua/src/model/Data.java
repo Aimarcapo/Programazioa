b4dd544bda17eb9992e320df3ed9d2c1d74feca5
Alonso Toledano <alonso.aimar@uni.lan>
2022-12-16 09:03:51 +0100

    si

--- UD1/1-Introduction/UD2/06-DatenProiektua/src/model/Data.java
+++ UD1/1-Introduction/UD2/06-DatenProiektua/src/model/Data.java
@@ -3,48 +3,55 @@ package model;
 import java.util.stream.IntStream;
 
 public class Data{
-private int[] day=IntStream.range(1, 32).toArray();
-private int[] month=IntStream.range(1, 12).toArray();
-private int[] year=IntStream.range(1900, 9999).toArray();;
+private int[] days=IntStream.range(1, 32).toArray();
+private int[] months=IntStream.range(1, 12).toArray();
+private int[] years=IntStream.range(1900, 9999).toArray();
+
+private int day;
+private int month;
+private int year;
 
 
 
 
 
-
-public Data(int[] i,int[] j,int[] k){
-this.year=i;
-this.month=j;
-this.day=k;
+public Data(int day ,int year ,int month){
+this.year= year;
+this.month= month;
+this.day= day;
 
 }
-public int[] getDay(){
+public int getDay(){
     return this.day;
 }
-public int[] getMonth(){
+public int getMonth(){
     return this.month;
 }
-public int[] getYear(){
+public int getYear(){
     return this.year;
 }
-public void setDay(int[] day){
+public void setDay(int day){
     this.day=day;
 }
-public void setMonth(int[] month){
+public void setMonth(int month){
     this.month=month;
 }
-public void setYear(int[] year){
+public void setYear(int year){
     this.year=year;
 }
 public String toString(){
     return day +"/"+month+"/"+year;
 }
-public void setDay(int i) {
-}
-public void setDate(int[] day,int[] month, int[] year){
+
+public void setDate(int day,int month, int year){
     this.day=day;
     this.month=month;
     this.year=year;
 
 }
+public  String getHilabeteaLetretan(){
+    String []letrak={"Urtarrila","Otsaila","Martxoa","Apirila","Maiatza","Ekaina","Uztaila","Urria","Azaroa","Abendua"};
+    return letrak[month-1];
+}
+
 }
