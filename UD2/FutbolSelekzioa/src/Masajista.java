392454ff333ef961c62c3752ac5062e031eaa4d0
Alonso Toledano <alonso.aimar@uni.lan>
2023-01-23 14:09:37 +0100

    2023/1/23

---
+++ UD2/FutbolSelekzioa/src/Masajista.java
@@ -0,0 +1,21 @@
+public class Masajista {
+    private String titulacion;
+    private int aniosexperiencia;
+    public Masajista(String titulacion, int aniosexperiencia) {
+        this.titulacion = titulacion;
+        this.aniosexperiencia = aniosexperiencia;
+    }
+    public String getTitulacion() {
+        return titulacion;
+    }
+    public void setTitulacion(String titulacion) {
+        this.titulacion = titulacion;
+    }
+    public int getAniosexperiencia() {
+        return aniosexperiencia;
+    }
+    public void setAniosexperiencia(int aniosexperiencia) {
+        this.aniosexperiencia = aniosexperiencia;
+    }
+    
+}
